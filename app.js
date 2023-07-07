519afa00054f2f09c8fc1f6c794ea4c59a784210
Keyzoth <corlays@outlook.com>
2023-07-07 23:46:43 +0200

    w xc
    sdv

--- app.js
+++ app.js
@@ -1,17 +1,65 @@
-const reponsiveMenuButton = document.getElementById('responsiveMenuToggleButton');
+const dropdownBtn = document.querySelectorAll(".dropdown-btn");
+const dropdown = document.querySelectorAll(".dropdown");
+const hamburgerBtn = document.getElementById("hamburger");
+const navMenu = document.querySelector(".menu");
+const links = document.querySelectorAll(".dropdown a");
 
-const navBarLinks = document.querySelector('.nav-links');
+function setAriaExpandedFalse() {
+  dropdownBtn.forEach((btn) => btn.setAttribute("aria-expanded", "false"));
+}
 
-reponsiveMenuButton.addEventListener('click', () => {
-    navBarLinks.classList.toggle('open');
-    reponsiveMenuButton.classList.toggle('open')
-})
+function closeDropdownMenu() {
+  dropdown.forEach((drop) => {
+    drop.classList.remove("active");
+    drop.addEventListener("click", (e) => e.stopPropagation());
+  });
+}
 
-const allNavLinks = document.querySelectorAll('.nav-links li');
+function toggleHamburger() {
+  navMenu.classList.toggle("show");
+}
 
-allNavLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        navBarLinks.classList.remove('open');
-        reponsiveMenuButton.classList.remove('open');
-    })
-})
+dropdownBtn.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    const dropdownIndex = e.currentTarget.dataset.dropdown;
+    const dropdownElement = document.getElementById(dropdownIndex);
+
+    dropdownElement.classList.toggle("active");
+    dropdown.forEach((drop) => {
+      if (drop.id !== btn.dataset["dropdown"]) {
+        drop.classList.remove("active");
+      }
+    });
+    e.stopPropagation();
+    btn.setAttribute(
+      "aria-expanded",
+      btn.getAttribute("aria-expanded") === "false" ? "true" : "false"
+    );
+  });
+});
+
+// close dropdown menu when the dropdown links are clicked
+links.forEach((link) =>
+  link.addEventListener("click", () => {
+    closeDropdownMenu();
+    setAriaExpandedFalse();
+    toggleHamburger();
+  })
+);
+
+// close dropdown menu when you click on the document body
+document.documentElement.addEventListener("click", () => {
+  closeDropdownMenu();
+  setAriaExpandedFalse();
+});
+
+// close dropdown when the escape key is pressed
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    closeDropdownMenu();
+    setAriaExpandedFalse();
+  }
+});
+
+// toggle hamburger menu
+hamburgerBtn.addEventListener("click", toggleHamburger);
